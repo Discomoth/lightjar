db840b84a961d3331c66178dfdc34a24d4d6ec29
Discomoth <raineyratcliff@gmail.com>
2024-12-02 17:30:55 -0800

    Added additional LED sequences

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def reset_leds():
         n[index] = (0,0,0)
 
 reset_leds()
+
 time_delay = 0.001
 
 def hex_to_rgb(hex_str):
@@ -30,6 +31,7 @@ cool_colors1 = [hex_to_rgb(x) for x in [
     '#a1aadc',
     '#b9baeb'
 ]]
+
 # Remap LEDs to handle alternating direction
 def remap_leds(neo_led_obj):
     final_list = []
@@ -52,9 +54,10 @@ def remap_leds(neo_led_obj):
     return final_list
 
 n.n_r = remap_leds(n)
+
 # Mode 1
 
-while True:
+while False:
     for led in range(led_count):
         for intensity in range(32):
             n[led] = (intensity*8, intensity*8, intensity*8)
@@ -95,3 +98,101 @@ while False:
             n[led-1] = (0,0,0)
             n.write()
             time.sleep(0.01)
+
+def test_seq_1():
+    leds = [x for x in n.n_r]
+    led_columns = []
+    for column in range(6):
+        col_list = []
+        for row in range(8):
+            col_list.append(leds.pop(0))
+        led_columns.append(col_list)
+    for led in range(8):
+        for column in led_columns:
+            n[column[led]] = (128,128,128)
+        n.write()
+        time.sleep(0.1)
+    for led in range(8):
+        for column in led_columns:
+            n[column[led]] = (0,0,0)
+        n.write()
+        time.sleep(0.1)
+        
+def sweeping_colors():
+    
+    colors = [x for x in palettes.gradient_2]
+    colors_rev = [x for x in palettes.gradient_2]
+    colors_rev.reverse()
+    
+    leds = [x for x in n.n_r]
+    led_columns = []
+    for column in range(6):
+        col_list = []
+        for row in range(8):
+            col_list.append(leds.pop(0))
+        led_columns.append(col_list)
+        
+    for color in colors:
+        for led in range(8):
+            for column in led_columns:
+                n[column[led]] = color
+            n.write()
+            time.sleep(0.05)
+    for color in colors_rev:
+        for led in range(8):
+            for column in led_columns:
+                n[column[led]] = color
+            n.write()
+            time.sleep(0.05)
+    
+
+# Sequence Functions
+def color_rain():
+    rain_gradient_map = [
+        [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.9, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.5, 0.9, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.2, 0.5, 0.9, 1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.1, 0.2, 0.5, 0.9, 1.0, 0.0, 0.0, 0.0],
+        [0.05, 0.1, 0.2, 0.5, 0.9, 1.0, 0.0, 0.0],
+        [0.01, 0.05, 0.1, 0.2, 0.5, 0.9, 1.0, 0.0],
+        [0.0, 0.01, 0.05, 0.1, 0.2, 0.5, 0.9, 1.0],
+        [0.0, 0.0, 0.01, 0.05, 0.1, 0.2, 0.5, 0.9],
+        [0.0, 0.0, 0.0, 0.01, 0.05, 0.1, 0.2, 0.5],
+        [0.0, 0.0, 0.0, 0.0, 0.01, 0.05, 0.1, 0.2],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.05, 0.1],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.05],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    ]
+    rand_col = random.randint(0, 5)
+    leds = n.n_r[int(rand_col * 8):int((rand_col * 8)+8)]
+    print(f"Col: {rand_col} - LEDs: {leds}")
+    rand_color = cool_colors1[random.randint(0, len(cool_colors1)-1)]
+    # Compute gradient mapping
+    gradient_values = []
+
+    for gradient in rain_gradient_map:
+        gradient_step = []
+        for led_mult in gradient:
+            led_values =(
+                int(rand_color[0]*led_mult),
+                int(rand_color[1]*led_mult),
+                int(rand_color[2]*led_mult))
+            gradient_step.append(led_values)
+
+        gradient_values.append(gradient_step)
+
+    for step in gradient_values:
+        for pos, led in enumerate(leds):
+            n[led] = step[pos]
+        n.write()
+        time.sleep(0.01)
+    time.sleep(random.randrange(0, 100)/100)
+
+while True:
+    try:
+        sweeping_colors()
+    except KeyboardInterrupt:
+        reset_leds()
+        break
